7d369da542beacb18258908a58b513b1a54b46e0
fantazer <fantazer@text.ru>
2016-01-03 19:30:44 +0300

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$(function() {
+      $(document).webicons({
+        svgSet: {
+          "flat-color-icons": '//cdn.rawgit.com/icons8/flat-color-icons/v1.0.2/icon-set/icons.svg'
+        },
+        alias: {
+          fci: 'flat-color-icons'
+        },
+        icon: {
+          clock: '//cdn.rawgit.com/icons8/flat-color-icons/v1.0.2/svg/clock.svg'
+        }
+      });
+    });	
+  
+})
